46a4a4694786f79d3cb5b06fd5a10244bd92e72c
Noam Aharony <aharonyn@gmail.com>
2020-07-30 09:26:14 +0300

    Changes from upstream.

---
+++ django_analyses/factories/__init__.py
@@ -0,0 +1,2 @@
+from .analysis import AnalysisFactory
+from .category import CategoryFactory
---
+++ django_analyses/factories/output/output_specification.py
@@ -0,0 +1,26 @@
+import factory
+
+from factory.django import DjangoModelFactory
+from django_analyses.factories.output.definitions.file_output_definition import (
+    FileOutputDefinitionFactory,
+)
+from django_analyses.factories.analysis import AnalysisFactory
+
+
+class OutputSpecificationFactory(DjangoModelFactory):
+    analysis = factory.SubFactory(AnalysisFactory)
+
+    class Meta:
+        model = "django_analyses.OutputSpecification"
+
+    @factory.post_generation
+    def base_output_definitions(self, create, extracted, **kwargs):
+        if create:
+            if extracted:
+                # A list of base_output_definitions was passed in, use it
+                for base_output_definition in extracted:
+                    self.base_output_definitions.add(base_output_definition)
+            else:
+                self.base_output_definitions.add(FileOutputDefinitionFactory())
+                self.base_output_definitions.add(FileOutputDefinitionFactory())
+                self.base_output_definitions.add(FileOutputDefinitionFactory())
